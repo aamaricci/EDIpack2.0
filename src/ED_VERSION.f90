eb81bb1ecdbac6729f75d8612597f4aa6164e045
Adriano Amaricci <adriano.amaricci@gmail.com>
2021-08-13 16:43:42 +0200

    0.2.2 PATCH
    CMake instruction updated with better handling of modules and versioning.

--- src/ED_VERSION.f90
+++ src/ED_VERSION.f90
@@ -1,4 +1,5 @@
 MODULE ED_VERSION
   implicit none
   !GIT VERSION
-  character(len=41),parameter,public :: version = "b8a019ce1dd9fcb17b622b9e8e494END MODULE ED_VERSION
+  character(len=41),parameter,public :: version = "0fbef9ef138b6ce212613195345d567a0e9b537a"
+END MODULE ED_VERSION
