3f797c4cb0a5f9b1a3ddb9f5208a1fe2092cec69
lcrippa <crippa.lorenzo@gmail.com>
2024-10-18 13:55:56 +0200

    moved custom modules to utils folder

--- doc/conf.py
+++ doc/conf.py
@@ -17,6 +17,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../src/python'))
 sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('./utils'))
 
 import recommonmark
 from recommonmark.transform import AutoStructify
