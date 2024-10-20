d01ee8db7d318fe6dde62845bbd0799894e15d8b
lcrippa <crippa.lorenzo@gmail.com>
2024-10-20 17:15:21 +0200

    minor fix

--- src/ED_IO/get_dens.f90
+++ src/ED_IO/get_dens.f90
@@ -1,6 +1,6 @@
 subroutine ed_get_dens_n0(self,iorb)
   real(8) :: self !The density value or array of values
-  integer,optional      :: iorb
+  integer,optional      :: iorb !the orbital index
   integer               :: iorb_
   iorb_=1;if(present(iorb))iorb_=iorb
   if(iorb_>Norb)stop "ed_get_dens error: orbital index > N_orbital"
@@ -10,7 +10,7 @@ end subroutine ed_get_dens_n0
 
 subroutine ed_get_dens_n1(self,iorb,Nlat)
   real(8),dimension(:) :: self
-  integer,optional     :: iorb !the orbital index
+  integer,optional     :: iorb 
   integer,optional     :: Nlat !the number of inequivalent impurity sites for real-space DMFT
   integer              :: iorb_
   iorb_=1;if(present(iorb))iorb_=iorb
