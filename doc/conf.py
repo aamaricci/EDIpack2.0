ee9c9326b167e636062b475203186cbd390b31c8
lcrippa <crippa.lorenzo@gmail.com>
2024-10-27 15:03:18 +0100

    Update:
    fix an annoying bug: mathjax (responsible for rendering the latex
    formulas) is not included in the html of the pages unless the :math:
    environment is used at least one in them. The sidebar doesn't count. So,
    in pages with only plain text, the title of the "chisquare fit" section
    in the sidebar was not rendered.
    Fixed by telling conf.py to include always all assets.

--- doc/conf.py
+++ doc/conf.py
@@ -239,3 +239,6 @@ epub_title = project
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ['search.html']
 
+#workaround: use mathjax on all pages
+def setup(app):
+    app.set_html_assets_policy('always')
